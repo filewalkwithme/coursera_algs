3c6067c39cd3073f86c2ba7a266af0fb64f785b9
maiconio <maiconscosta@gmail.com>
2017-07-21 00:50:26 +0200

    KdTree: final sollution (100%)

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -22,16 +22,31 @@ public class KdTree {
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (p == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        int inserted = 0;
         if (node == null) {
             node = new Node(p);
+            inserted = 1;
         } else {
-            node.insert(p);
+            inserted = node.insert(p);
         }
+        size = size + inserted;
+
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return this.node.find(p) != null;
+        if (p == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        if (this.node != null) {
+            return this.node.find(p) != null;
+        }
+        return false;
     }
 
     // draw all points to standard draw
@@ -43,203 +58,210 @@ public class KdTree {
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
 
         TreeSet<Point2D> result = new TreeSet<Point2D>();
 
         if (this.node != null) {
             this.node.range(rect, result);
         }
-
         return result;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
-    public Point2D nearest(Point2D p) {
-        Point2D nearestPoint = null;
-        boolean firstStep = true;
-        double smallerDistance = 0;
-
-        this.node.nearest(p, this.node.getPoint(), this.node.getPoint().distanceTo(p));
-
-        return nearestPoint;
-    }
-
-    // unit testing of the methods (optional)
-    public static void main(String[] args) {
-        System.out.printf("oi!\n");
-        Node n = new Node(new Point2D(5,5));
-        n.insert(new Point2D(4,4));
-        n.insert(new Point2D(4,6));
-        n.insert(new Point2D(3,3));
-        n.insert(new Point2D(3,1));
-        n.insert(new Point2D(7,7));
-        n.draw();
-
-        System.out.printf("Contains 4,6? %b\n", n.find(new Point2D(4,6))!=null);
-        System.out.printf("Contains 8,8? %b\n", n.find(new Point2D(8,8))!=null);
-    }
-}
-
-class Node {
-    public int level;
-    public Node left;
-    public Node right;
-    private Point2D point;
-
-    Node(Point2D point){
-        this.point = point;
-    }
-
-    public Point2D getPoint(){
-        return this.point;
-    }
-
-
-    public void draw() {
-        if (this.point != null) {
-            this.point.draw();
-            //if (level % 2 == 0){
-                this.point.drawTo(new Point2D(0, this.point.y()));
-            //}
-
-            if (this.left != null) {
-                this.left.draw();
-            }
-
-            if (this.right != null) {
-                this.right.draw();
-            }
-        }
-    }
-
-    public void nearest(Point2D queryPoint, Point2D nearest, double nearestDistance) {
-        if (this.point != null && this.point.distanceTo(queryPoint) < nearestDistance) {
-            nearest = this.point;
-            nearestDistance = this.point.distanceTo(queryPoint);
+    public Point2D nearest(Point2D queryPoint) {
+        if (queryPoint == null) {
+            throw new java.lang.IllegalArgumentException();
         }
 
-        if (level % 2 == 0) {
-            if (this.left != null && queryPoint.distanceTo(new Point2D(this.left.point.x(), queryPoint.y())) < nearestDistance) {
-                this.left.nearest(queryPoint, nearest, nearestDistance);
-            }
-            if (this.right != null && queryPoint.distanceTo(new Point2D(this.right.point.x(), queryPoint.y())) < nearestDistance) {
-                this.right.nearest(queryPoint, nearest, nearestDistance);
-            }
-        } else {
-            if (this.left != null && queryPoint.distanceTo(new Point2D(queryPoint.x(), this.left.point.y())) < nearestDistance) {
-                this.left.nearest(queryPoint, nearest, nearestDistance);
-            }
-            if (this.right != null && queryPoint.distanceTo(new Point2D(queryPoint.x(), this.right.point.y())) < nearestDistance) {
-                this.right.nearest(queryPoint, nearest, nearestDistance);
-            }
+        if (this.node != null && this.node.getPoint() != null) {
+            Point2D nearestPoint = new Point2D(this.node.getPoint().x(), this.node.getPoint().y());
+            nearestPoint = this.node.nearest(queryPoint, nearestPoint, nearestPoint.distanceTo(queryPoint));
+
+            return nearestPoint;
         }
+        return null;
     }
 
-    public void range(RectHV rect, TreeSet<Point2D> result) {
-        if (rect.contains(this.point)) {
-            result.add(this.point);
-            if (level % 2 == 0) {
-                if (this.left != null && rect.xmin() <= this.point.x() || rect.xmax() <= this.point.x()) {
-                    this.left.range(rect, result);
-                }
-                if (this.right != null && rect.xmin() >= this.point.x() || rect.xmax() >= this.point.x()) {
-                    this.right.range(rect, result);
-                }
-            } else {
-                if (this.left != null && rect.ymin() <= this.point.y() || rect.ymax() <= this.point.y()) {
-                    this.left.range(rect, result);
-                }
-                if (this.right != null && rect.ymin() >= this.point.y() || rect.ymax() >= this.point.y()) {
-                    this.right.range(rect, result);
-                }
-            }
+    private class Node {
+        private int level;
+        private Node left;
+        private Node right;
+        private final Point2D point;
+
+        Node(Point2D point) {
+            this.point = point;
         }
-    }
 
-
-    public void print() {
-        if (this.point != null) {
-            String traces = "";
-            for (int i = 0; i < level * 4; i++) {
-                traces = traces + "-";
-            }
-            System.out.printf(traces+"%f|%f\n", this.point.x(), this.point.y());
-            if (this.left != null) {
-                this.left.print();
-            }
-            if (this.right != null) {
-                this.right.print();
-            }
-        }
-    }
-
-    public void insert(Point2D point) {
-        if (level % 2 == 0) {
-            if (point.x() < this.point.x()) {
-                if (this.left == null) {
-                    this.left = new Node(point);
-                    this.left.level = this.level + 1;
-                } else {
-                    this.left.insert(point);
-                }
-            } else {
-                if (this.right == null) {
-                    this.right = new Node(point);
-                    this.right.level = this.level + 1;
-                } else {
-                    this.right.insert(point);
-                }
-            }
-        } else {
-            if (point.y() < this.point.y()) {
-                if (this.left == null) {
-                    this.left = new Node(point);
-                    this.left.level = this.level + 1;
-                } else {
-                    this.left.insert(point);
-                }
-            } else {
-                if (this.right == null) {
-                    this.right = new Node(point);
-                    this.right.level = this.level + 1;
-                } else {
-                    this.right.insert(point);
-                }
-            }
-        }
-    }
-
-
-    public Point2D find(Point2D point) {
-        if (point.x() == this.point.x() && point.y() == this.point.y()) {
+        public Point2D getPoint() {
             return this.point;
-        } else {
+        }
+
+
+        public void draw() {
+            if (this.point != null) {
+                this.point.draw();
+                this.point.drawTo(new Point2D(0, this.point.y()));
+
+                if (this.left != null) {
+                    this.left.draw();
+                }
+
+                if (this.right != null) {
+                    this.right.draw();
+                }
+            }
+        }
+
+        public Point2D nearest(Point2D queryPoint, Point2D nearest, double nearestDistance) {
+            if (this.point != null && this.point.distanceTo(queryPoint) < nearestDistance) {
+                nearest = this.point;
+                nearestDistance = this.point.distanceTo(queryPoint);
+            }
+
             if (level % 2 == 0) {
-                if (point.x() < this.point.x()) {
-                    if (this.left == null) {
-                        return null;
-                    } else {
-                        return this.left.find(point);
+                if (queryPoint.x() < this.point.x()) {
+                    if (this.left != null) {
+                        nearest = this.left.nearest(queryPoint, nearest, nearestDistance);
+                        nearestDistance = nearest.distanceTo(queryPoint);
+                    }
+                    if (this.right != null && nearestDistance > queryPoint.distanceTo(new Point2D(this.point.x(), queryPoint.y()))) {
+                        nearest = this.right.nearest(queryPoint, nearest, nearestDistance);
+                        nearestDistance = nearest.distanceTo(queryPoint);
                     }
                 } else {
-                    if (this.right == null) {
-                        return null;
-                    } else {
-                        return this.right.find(point);
+                    if (this.right != null) {
+                        nearest = this.right.nearest(queryPoint, nearest, nearestDistance);
+                        nearestDistance = nearest.distanceTo(queryPoint);
+                    }
+                    if (this.left != null && nearestDistance > queryPoint.distanceTo(new Point2D(this.point.x(), queryPoint.y()))) {
+                        nearest = this.left.nearest(queryPoint, nearest, nearestDistance);
+                        nearestDistance = nearest.distanceTo(queryPoint);
                     }
                 }
             } else {
-                if (point.y() < this.point.y()) {
+                if (queryPoint.y() < this.point.y()) {
+                    if (this.left != null) {
+                        nearest = this.left.nearest(queryPoint, nearest, nearestDistance);
+                        nearestDistance = nearest.distanceTo(queryPoint);
+                    }
+                    if (this.right != null && nearestDistance > queryPoint.distanceTo(new Point2D(queryPoint.x(), this.point.y()))) {
+                        nearest = this.right.nearest(queryPoint, nearest, nearestDistance);
+                        nearestDistance = nearest.distanceTo(queryPoint);
+                    }
+                } else {
+                    if (this.right != null) {
+                        nearest = this.right.nearest(queryPoint, nearest, nearestDistance);
+                        nearestDistance = nearest.distanceTo(queryPoint);
+                    }
+                    if (this.left != null && nearestDistance > queryPoint.distanceTo(new Point2D(queryPoint.x(), this.point.y()))) {
+                        nearest = this.left.nearest(queryPoint, nearest, nearestDistance);
+                        nearestDistance = nearest.distanceTo(queryPoint);
+                    }
+                }
+            }
+            return nearest;
+        }
+
+        public void range(RectHV rect, TreeSet<Point2D> result) {
+            if (rect.contains(this.point)) {
+                result.add(this.point);
+            }
+
+            if (level % 2 == 0) {
+                if (this.left != null && (rect.xmin() <= this.point.x() || rect.xmax() <= this.point.x())) {
+                    this.left.range(rect, result);
+                }
+                if (this.right != null && (rect.xmin() >= this.point.x() || rect.xmax() >= this.point.x())) {
+                    this.right.range(rect, result);
+                }
+            } else {
+                if (this.left != null && (rect.ymin() <= this.point.y() || rect.ymax() <= this.point.y())) {
+                    this.left.range(rect, result);
+                }
+                if (this.right != null && (rect.ymin() >= this.point.y() || rect.ymax() >= this.point.y())) {
+                    this.right.range(rect, result);
+                }
+            }
+
+        }
+
+        public int insert(Point2D p) {
+            if (this.point.compareTo(p) == 0) {
+                return 0;
+            }
+            if (level % 2 == 0) {
+                if (p.x() < this.point.x()) {
                     if (this.left == null) {
-                        return null;
+                        this.left = new Node(p);
+                        this.left.level = this.level + 1;
+                        return 1;
                     } else {
-                        return this.left.find(point);
+                        return this.left.insert(p);
                     }
                 } else {
                     if (this.right == null) {
-                        return null;
+                        this.right = new Node(p);
+                        this.right.level = this.level + 1;
+                        return 1;
                     } else {
-                        return this.right.find(point);
+                        return this.right.insert(p);
+                    }
+                }
+            } else {
+                if (p.y() < this.point.y()) {
+                    if (this.left == null) {
+                        this.left = new Node(p);
+                        this.left.level = this.level + 1;
+                        return 1;
+                    } else {
+                        return this.left.insert(p);
+                    }
+                } else {
+                    if (this.right == null) {
+                        this.right = new Node(p);
+                        this.right.level = this.level + 1;
+                        return 1;
+                    } else {
+                        return this.right.insert(p);
+                    }
+                }
+            }
+        }
+
+        public Point2D find(Point2D point) {
+            if (point.x() == this.point.x() && point.y() == this.point.y()) {
+                return this.point;
+            } else {
+                if (level % 2 == 0) {
+                    if (point.x() < this.point.x()) {
+                        if (this.left == null) {
+                            return null;
+                        } else {
+                            return this.left.find(point);
+                        }
+                    } else {
+                        if (this.right == null) {
+                            return null;
+                        } else {
+                            return this.right.find(point);
+                        }
+                    }
+                } else {
+                    if (point.y() < this.point.y()) {
+                        if (this.left == null) {
+                            return null;
+                        } else {
+                            return this.left.find(point);
+                        }
+                    } else {
+                        if (this.right == null) {
+                            return null;
+                        } else {
+                            return this.right.find(point);
+                        }
                     }
                 }
             }
--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.RectHV;
 import java.util.TreeSet;
 
 public class PointSET {
-    TreeSet<Point2D> redBlackTree;
+    private final TreeSet<Point2D> redBlackTree;
 
     // construct an empty set of points
     public PointSET() {
@@ -22,11 +22,17 @@ public class PointSET {
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (p == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
         redBlackTree.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
+        if (p == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
         return redBlackTree.contains(p);
     }
 
@@ -39,6 +45,10 @@ public class PointSET {
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
         TreeSet<Point2D> result = new TreeSet<Point2D>();
         for (Point2D p: redBlackTree) {
             if (rect.contains(p)) {
@@ -51,31 +61,27 @@ public class PointSET {
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
+        if (p == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
         Point2D nearestPoint = null;
         boolean firstStep = true;
         double smallerDistance = 0;
         for (Point2D currentPoint: redBlackTree) {
-            if (currentPoint == p) {
-                continue;
-            }
-            if (firstStep = true) {
+            if (firstStep) {
                 firstStep = false;
-                nearestPoint = p;
+                nearestPoint = currentPoint;
                 smallerDistance = currentPoint.distanceTo(p);
             } else {
                 double distance = currentPoint.distanceTo(p);
                 if (distance < smallerDistance) {
-                    nearestPoint = p;
+                    nearestPoint = currentPoint;
                     smallerDistance = distance;
                 }
             }
         }
 
         return nearestPoint;
-    }
-
-    // unit testing of the methods (optional)
-    public static void main(String[] args) {
-
     }
 }
